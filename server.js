c3d4e762f2d257c09789b854568067390c7babde
Mery Orellanam <orellana_mery@hotmail.com>
2024-04-24 12:44:31 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { getCustomers } = require('./data-acess');
 const app = express();
 const port = 4000;
 
 
 app.use(express.static('public'));
+app.get("/customers", async (req,res)=>{
+    const customers = await getCustomers();
+    res.send(customers);
+})
 console.log("Open a browser to http://localhost:"+port+" to view the application");
 app.listen(port);
